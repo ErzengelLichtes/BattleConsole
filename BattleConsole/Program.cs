038caf0e0eb12adb0018de785ab51535928f0a5f
Stephen Coman <scoman91@gmail.com>
2020-01-29 20:50:56 -0800

    Added testing commands.

--- BattleConsole/Program.cs
+++ BattleConsole/Program.cs
@@ -16,16 +16,33 @@ namespace BattleConsole
             string enemyHealthBar = new string('|', enemyHealth / 10);
             string enemyStaminaBar = new string('|', enemyStamina / 10);
 
-            //Display character cards.
-            Console.WriteLine("Player");
-            Console.WriteLine($"Health: {playerHealthBar} {playerHealth}");
-            Console.WriteLine($"Stamina: {playerStaminahBar} {playerStamina}");
-            Console.WriteLine();
-            Console.WriteLine("Enemy");
-            Console.WriteLine($"Health: {enemyHealthBar} {enemyHealth}");
-            Console.WriteLine($"Stamina: {enemyStaminaBar} {enemyStamina}");
 
-
+            bool active = true;
+            while (active)
+            {
+                //Display character cards.
+                Console.WriteLine("Player");
+                Console.WriteLine($"Health: {playerHealthBar} {playerHealth}");
+                Console.WriteLine($"Stamina: {playerStaminahBar} {playerStamina}");
+                Console.WriteLine();
+                Console.WriteLine("Enemy");
+                Console.WriteLine($"Health: {enemyHealthBar} {enemyHealth}");
+                Console.WriteLine($"Stamina: {enemyStaminaBar} {enemyStamina}");
+                
+                var keyInfo = Console.ReadKey(true);
+                switch (keyInfo.Key)
+                {
+                    case ConsoleKey.H:
+                        playerHealth -= 1;
+                        break;
+                    case ConsoleKey.J:
+                        playerStamina -= 1;
+                        break;
+                    case ConsoleKey.Escape:
+                        active = false;
+                        break;
+                }
+            }
 
 
         }
